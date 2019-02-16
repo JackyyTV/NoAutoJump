a260ed0727d4b1a869ff5808af822444fd401a67
Jacky Scrubby <jackyytv@gmail.com>
2019-02-17 02:48:44 +0800

    1.13 port

--- src/main/java/jackyy/noautojump/NoAutoJump.java
+++ src/main/java/jackyy/noautojump/NoAutoJump.java
@@ -1,42 +1,40 @@
 package jackyy.noautojump;
 
+import net.minecraft.client.GameSettings;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiControls;
 import net.minecraft.client.gui.GuiOptionButton;
-import net.minecraft.client.settings.GameSettings;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = NoAutoJump.MODID, version = NoAutoJump.VERSION, name = NoAutoJump.MODNAME, certificateFingerprint = "@FINGERPRINT@", acceptedMinecraftVersions = NoAutoJump.MCVERSION, clientSideOnly = true, acceptableRemoteVersions = "*", useMetadata = true)
+@Mod(NoAutoJump.MODID)
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class NoAutoJump {
 
-    public static final String VERSION = "1.1";
-    public static final String MCVERSION = "[1.12,1.13)";
     public static final String MODID = "noautojump";
     public static final String MODNAME = "No Auto Jump";
     public static final Logger LOGGER = LogManager.getLogger(MODNAME);
 
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
+    public NoAutoJump() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @SubscribeEvent
+    @SubscribeEvent @OnlyIn(Dist.CLIENT)
     public void onClientTick(TickEvent.ClientTickEvent event) {
         if (event.phase == TickEvent.Phase.END) {
-            Minecraft.getMinecraft().gameSettings.autoJump = false;
+            Minecraft.getInstance().gameSettings.autoJump = false;
         }
     }
 
-    @SubscribeEvent
+    @SubscribeEvent @OnlyIn(Dist.CLIENT)
     public void onInitGui(GuiScreenEvent.InitGuiEvent.Post event) {
         if (event.getGui() instanceof GuiControls) {
             for (GuiButton button : event.getButtonList()) {
@@ -45,11 +43,6 @@ public class NoAutoJump {
                 }
             }
         }
-    }
-
-    @Mod.EventHandler
-    public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
-        LOGGER.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been modified. This will NOT be supported by the mod author!");
     }
 
 }
