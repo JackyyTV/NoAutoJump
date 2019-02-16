b7a5c7eee541799f908a55cc220ca56ca68796ef
Jacky Scrubby <jackyytv@gmail.com>
2019-02-17 01:10:15 +0800

    Limited version

--- src/main/java/jackyy/noautojump/NoAutoJump.java
+++ src/main/java/jackyy/noautojump/NoAutoJump.java
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 public class NoAutoJump {
 
     public static final String VERSION = "1.0";
-    public static final String MCVERSION = "[1.10,)";
+    public static final String MCVERSION = "[1.10,1.11)";
     public static final String MODID = "noautojump";
     public static final String MODNAME = "No Auto Jump";
     public static final Logger LOGGER = LogManager.getLogger(MODNAME);
