386d1759301952651d36876ebae1b1940f1841b3
Jacky <jackyytv@gmail.com>
2023-05-23 03:48:09 +0800

    1.16.5 port

--- src/main/java/jackyy/noautojump/NoAutoJump.java
+++ src/main/java/jackyy/noautojump/NoAutoJump.java
@@ -1,45 +1,48 @@
 package jackyy.noautojump;
 
-import net.minecraft.client.GameSettings;
+import net.minecraft.client.AbstractOption;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiControls;
-import net.minecraft.client.gui.GuiOptionButton;
+import net.minecraft.client.gui.screen.ControlsScreen;
+import net.minecraft.client.gui.widget.Widget;
+import net.minecraft.client.gui.widget.button.OptionButton;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(NoAutoJump.MODID)
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class NoAutoJump {
 
     public static final String MODID = "noautojump";
-    public static final String MODNAME = "No Auto Jump";
-    public static final Logger LOGGER = LogManager.getLogger(MODNAME);
 
     public NoAutoJump() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ModConfigs.SPEC);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent @OnlyIn(Dist.CLIENT)
     public void onClientTick(TickEvent.ClientTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            Minecraft.getInstance().gameSettings.autoJump = false;
+        if (ModConfigs.CONFIG.enableMod.get()) {
+            if (event.phase == TickEvent.Phase.END) {
+                Minecraft.getInstance().gameSettings.autoJump = false;
+            }
         }
     }
 
     @SubscribeEvent @OnlyIn(Dist.CLIENT)
     public void onInitGui(GuiScreenEvent.InitGuiEvent.Post event) {
-        if (event.getGui() instanceof GuiControls) {
-            for (GuiButton button : event.getButtonList()) {
-                if (button instanceof GuiOptionButton && ((GuiOptionButton) button).getOption() == GameSettings.Options.AUTO_JUMP) {
-                    button.enabled = false;
+        if (ModConfigs.CONFIG.enableMod.get()) {
+            if (event.getGui() instanceof ControlsScreen) {
+                for (Widget button : event.getWidgetList()) {
+                    if (button instanceof OptionButton && ((OptionButton) button).getOptions() == AbstractOption.AUTO_JUMP) {
+                        button.active = false;
+                    }
                 }
             }
         }
